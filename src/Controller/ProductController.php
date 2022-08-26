fb9066d17f2ce4a87456e6b7c39d5e23b93d5634
kylian97 <kyky972@icloud.com>
2022-08-25 21:51:14 -0400

    relation produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this ->entityManager = $entityManager;
+    }
+
+    #[Route('/nos-produits', name: 'app_products')]
     public function index(): Response
     {
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products'=>$products
+        ]);
+    }
+
+    #[Route('/produit/{slug}', name: 'app_product')]
+    public function show($slug): Response
+    {
+
+
+        $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+
+        if (!$product) {
+            return $this->redirectToRoute('products');
+        }
+
+
+        return $this->render('product/show.html.twig', [
+            'product'=>$product
         ]);
     }
 }
