947d2edc4044c4aa33725b4101f30206e8cd2029
kylian97 <kyky972@icloud.com>
2022-09-01 14:26:08 -0400

    page contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,23 +6,20 @@ use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailerInterface): Response
     {
         $contact = new Contact();
-
-        if ($this->getUser()) {
-            $contact->setFirstName($this->getUser()->getFirstName())
-            ->setLastName($this->getUser()->getLastName())
-            ->setEmail($this->getUser()->getEmail());
-        }
 
         $form = $this -> createForm(ContactType::class, $contact);
 
@@ -32,16 +29,33 @@ class ContactController extends AbstractController
 
            
             $contact = $form -> getData();
-
             $entityManager -> persist ($contact);
-
             $entityManager->flush();
+
+            //Email 
+
+            $email = (new TemplatedEmail())
+            ->from($contact->getEmail())
+            ->to('admin@dronamada.com')
+            ->subject($contact->getSubject())
+             // path of the Twig template to render
+            ->htmlTemplate('emails/contact.html.twig')
+
+            // pass variables (name => value) to the template
+             ->context([
+            'contact' => $contact
+            ])
+            ;
+
+           $mailerInterface->send($email);
 
             $this->addFlash(
             'success',
             'Votre demandé a été envoyé avec succès'
 
             );
+
+            return $this->redirectToRoute('app_contact');
             
 
              // dd($form->getData());
