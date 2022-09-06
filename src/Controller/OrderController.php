ddc5936c04d0393b6fadac78f936a401dde8b09d
kylian97 <kyky972@icloud.com>
2022-09-06 11:07:15 -0400

    page confirmation paiement

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Form\OrderType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OrderController extends AbstractController
 {
     #[Route('/commande', name: 'app_order')]
-    public function index(): Response
+    public function index(Cart $cart, Request $request): Response
     {
 
         // dd($this->getUser()->getAddresses()->getValues());
@@ -26,9 +28,16 @@ class OrderController extends AbstractController
             'user' => $this->getUser()
         ]);
 
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            dd($form->getData());
+        }
+
         return $this->render('order/index.html.twig', [
 
-            'form' => $form-> createView()
+            'form' => $form-> createView(),
+            'cart' => $cart -> getFull()
         ]);
     }
 }
--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use App\Entity\Address;
+use App\Entity\Carrier;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -32,6 +34,20 @@ class OrderType extends AbstractType
                 'multiple' => false,
                 'expanded' => true
             ])
+            ->add('carriers', EntityType::class, [
+
+                'label' => 'Choisissez votre transporteur',
+                'required' => true,
+                'class' => Carrier::class,
+                'multiple' => false,
+                'expanded' => true
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider ma commande',
+                'attr' => [
+                    'class' => 'btn btn-success btn-block'
+                ]
+            ] )
         ;
     }
 
